4ef07dbf64d164ba26e1ee8cb6476fbb59a3b6b5
Richard791-rucdary <badgcheets@gmail.com>
2026-08-26 23:35:29 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- main.go
+++ main.go
@@ -5,18 +5,23 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"log"
+	"math/big"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/resend/resend-go/v2"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 )
 
+//Definition of global variables and structs.
+
 var collection *mongo.Collection
 var coll *mongo.Collection
+var otpCon *mongo.Collection
 
 type rec struct {
 	ID      bson.ObjectID `json:"id" bson:"_id,omitempty"`
@@ -40,64 +45,79 @@ type Cust struct {
 	Exp      int64         `json:"exp" bson:"exp"`
 }
 
+// Load user data
 func loadData(write http.ResponseWriter, read *http.Request) {
 	write.Header().Set("Access-Control-Allow-Origin", "*")
 	write.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	write.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+
 	if read.Method == "OPTIONS" {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	type cus struct {
 		Name  string `json:"name"`
 		Token string `json:"token"`
 	}
-	defer read.Body.Close()
+
 	var vale []rec
 	var vales Cust
 	var res cus
+
 	err := json.NewDecoder(read.Body).Decode(&res)
 	if err != nil {
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid values!"})
 		return
 	}
+
 	defer read.Body.Close()
+
 	name := res.Name
 	token := res.Token
+	compareToken := makeToken(name, read)
 	err = collection.FindOne(read.Context(), bson.M{"useName": name}).Decode(&vales)
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Your internet is disconnected! Please Try again"})
 		return
 	}
+
 	if token == "" {
 		write.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Unable to authorise user. Please try again."})
 		return
 	}
-	if token != vales.Token {
+
+	if token != compareToken {
 		write.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Unable to authorise user. Please try again."})
 		return
 	}
 
 	cursor, err := coll.Find(read.Context(), bson.M{"useName": name})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Error connecting to Database! Please try again."})
 		return
 	}
+
 	defer cursor.Close(read.Context())
+
 	for cursor.Next(read.Context()) {
 		var est rec
 		cursor.Decode(&est)
 		vale = append(vale, est)
 	}
+
 	if err = cursor.Err(); err != nil {
 		write.Header().Set("Content-Type", "application/json")
 		write.WriteHeader(http.StatusInternalServerError)
@@ -106,13 +126,16 @@ func loadData(write http.ResponseWriter, read *http.Request) {
 	}
 
 	write.Header().Set("Content-Type", "application/json")
+
 	err = json.NewEncoder(write).Encode(map[string][]rec{"message": vale})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
+// User Sign-Up
 func signUp(write http.ResponseWriter, read *http.Request) {
 	write.Header().Set("Access-Control-Allow-Origin", "*")
 	write.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -122,6 +145,7 @@ func signUp(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
@@ -129,6 +153,7 @@ func signUp(write http.ResponseWriter, read *http.Request) {
 	}
 
 	defer read.Body.Close()
+
 	type users struct {
 		User     string `json:"user"`
 		Name     string `json:"name"`
@@ -137,48 +162,154 @@ func signUp(write http.ResponseWriter, read *http.Request) {
 
 	var vex users
 	var vexe Cust
+
 	err := json.NewDecoder(read.Body).Decode(&vex)
+
 	if err != nil {
 		write.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid data! Please input proper data."})
 		return
 	}
-	vim, err := bcrypt.GenerateFromPassword([]byte(vex.Password), 12)
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Poor internet connection! Please try again."})
 		return
 	}
-	token := makeToken(vex.User, read)
+
 	err = collection.FindOne(read.Context(), bson.M{"useName": vex.User}).Decode(&vexe)
+
 	if err == nil {
 		write.WriteHeader(http.StatusExpectationFailed)
-		json.NewEncoder(write).Encode(map[string]string{"err": "The Inputted username already exists. If trying to login, click login instead."})
+		json.NewEncoder(write).Encode(map[string]string{"err": "The Inputted e-mail already exists. If trying to login, click login instead."})
 		return
 	}
+
+	van := genOTP(vex.User, vex.Name, vex.Password)
+
+	write.WriteHeader(http.StatusOK)
+	json.NewEncoder(write).Encode(map[string]string{"message": van})
+	if err == nil {
+		write.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(write).Encode(map[string]string{"err": "Poor internet connection. Please check your internet connection."})
+		return
+	}
+
+	if err != nil {
+		write.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(write).Encode(map[string]string{"err": "Poor Internet connection. Please check your internet."})
+	}
+}
+
+// Confirm OTP for user Sign-Up
+func confOTP(write http.ResponseWriter, read *http.Request) {
+	write.Header().Set("Access-Control-Allow-Origin", "*")
+	write.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	write.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+
+	if read.Method == "OPTIONS" {
+		write.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if read.Method != "POST" {
+		write.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
+		return
+	}
+
+	type userp struct {
+		User  string    `bson:"user"`
+		OTP   string    `bson:"otp"`
+		Pass  string    `bson:"pass"`
+		Name  string    `bson:"name"`
+		Exp   time.Time `bson:"exp"`
+		Tries int       `bson:"tries"`
+	}
+
+	type OTP struct {
+		Email string `json:"email"`
+		OTP   string `json:"otp"`
+	}
+	var vex userp
+	var vexer OTP
+
+	err := json.NewDecoder(read.Body).Decode(&vexer)
+
+	if err != nil {
+		write.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid JSON. Please input valid JSON."})
+		return
+	}
+
+	defer read.Body.Close()
+
+	err = otpCon.FindOne(read.Context(), bson.M{"email": vexer.OTP}).Decode(&vex)
+
+	if err != nil {
+		write.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured. Please check ypur internet or input a valid email."})
+		return
+	}
+
+	if vex.Tries >= 5 {
+		write.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(write).Encode(map[string]string{"err": "You can't try again. Contact customer care for support."})
+		return
+	}
+
+	if vexer.OTP != vex.OTP {
+		write.WriteHeader(http.StatusUnauthorized)
+		_, _ = otpCon.UpdateOne(read.Context(), bson.M{"email": vex.User}, bson.M{"$set": bson.M{"tries": vex.Tries + 1}})
+		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid OTP. you have " + string(5-vex.Tries+1) + " tries left."})
+		return
+	}
+
+	vim, err := bcrypt.GenerateFromPassword([]byte(vex.Pass), 12)
+
+	if err != nil {
+		write.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(write).Encode(map[string]string{"err": "Poor internet connection. Connect to stronger internet!"})
+		return
+	}
+
+	make := makeToken(vex.User, read)
+
 	saves := Cust{
 		UseName:  vex.User,
 		RealName: vex.Name,
 		IsPaid:   7,
 		Password: string(vim),
 		PayDate:  int64(time.Now().UnixMilli()),
-		Token:    token,
+		Token:    make,
 		Exp:      int64(time.Now().UnixMilli() + 86400000),
 	}
+
 	_, err = collection.InsertOne(read.Context(), saves)
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured. Please try again"})
 		return
 	}
+	vant := makeToken(vex.User, read)
+	if _, err = collection.UpdateOne(read.Context(), bson.M{"useName": vex.User}, bson.M{"$set": bson.M{"token": vant}}); err != nil {
+		write.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured. Please reload."})
+		return
+	}
 	write.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(write).Encode(map[string]string{"message": "Success"})
+
+	err = json.NewEncoder(write).Encode(map[string]string{"message": "OTP confirmation was a success"})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured. Please try again"})
 		return
 	}
 }
+
+// User Log-In into account
 func logIn(write http.ResponseWriter, read *http.Request) {
 	write.Header().Set("Access-Control-Allow-Origin", "*")
 	write.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -188,12 +319,15 @@ func logIn(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	defer read.Body.Close()
+
 	type users struct {
 		User     string `json:"user"`
 		Password string `json:"password"`
@@ -201,7 +335,9 @@ func logIn(write http.ResponseWriter, read *http.Request) {
 
 	var vex users
 	var vexer Cust
+
 	err := json.NewDecoder(read.Body).Decode(&vex)
+
 	if err != nil {
 		write.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid data! Input proper data."})
@@ -209,21 +345,28 @@ func logIn(write http.ResponseWriter, read *http.Request) {
 	}
 
 	err = collection.FindOne(read.Context(), bson.M{"useName": vex.User}).Decode(&vexer)
+
 	if err != nil {
 		write.WriteHeader(404)
 		json.NewEncoder(write).Encode(map[string]string{"err": "User not found. If u want to signup, click signup instead."})
 		return
 	}
+
 	err = bcrypt.CompareHashAndPassword([]byte(vexer.Password), []byte(vex.Password))
+
 	if err != nil {
 		write.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid Password. Try password again"})
 		return
 	}
+
 	write.Header().Set("Content-Type", "application/json")
+
 	write.WriteHeader(http.StatusOK)
 	token := makeToken(vex.User, read)
+
 	err = json.NewEncoder(write).Encode(map[string]string{"token": token, "name": vexer.RealName})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! Please try again."})
@@ -240,16 +383,19 @@ func retPay(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "GET" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	type Price struct {
 		Price  float64 `json:"price"`
 		Period string  `json:"period"`
 		Days   int     `json:"days"`
 	}
+
 	prices := []Price{
 		{274.99, "1 week", 7},
 		{996.99, "1 month", 30},
@@ -257,8 +403,10 @@ func retPay(write http.ResponseWriter, read *http.Request) {
 		{10257.99, "1 year", 365},
 		{19919.99, "2 years", 732},
 	}
+
 	write.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(write).Encode(prices)
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! Please try again."})
@@ -275,31 +423,40 @@ func regPes(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	defer read.Body.Close()
+
 	type reg struct {
 		User string `json:"user"`
 		Days int    `json:"days"`
 	}
+
 	var res reg
+
 	err := json.NewDecoder(read.Body).Decode(&res)
+
 	if err != nil {
 		write.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid data! Please input proper data"})
 		return
 	}
+
 	filter := bson.M{"useName": res.User}
 	update := bson.M{"$set": bson.M{"isPaid": res.Days, "payDate": int64(time.Now().UnixMilli())}}
 	result, err := collection.UpdateOne(read.Context(), filter, update)
+
 	if result.MatchedCount == 0 {
 		write.WriteHeader(http.StatusConflict)
 		json.NewEncoder(write).Encode(map[string]string{"err": "User does not exist!"})
 		return
 	}
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An Error occured! Please try again."})
@@ -307,6 +464,7 @@ func regPes(write http.ResponseWriter, read *http.Request) {
 	}
 	write.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(write).Encode(map[string]string{"message": "Success"})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An Error occured! Please try again."})
@@ -323,11 +481,13 @@ func delete(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	defer read.Body.Close()
 
 	type reco struct {
@@ -335,8 +495,10 @@ func delete(write http.ResponseWriter, read *http.Request) {
 		Token string `json:"token"`
 		Name  string `json:"name"`
 	}
+
 	var ort reco
 	err := json.NewDecoder(read.Body).Decode(&ort)
+
 	if err != nil {
 		write.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid Data! Input proper data."})
@@ -350,13 +512,17 @@ func delete(write http.ResponseWriter, read *http.Request) {
 		json.NewEncoder(write).Encode(map[string]string{"err": "We were unable to authorise you. Please try again"})
 		return
 	}
+
 	id, err := bson.ObjectIDFromHex(ort.ID)
+
 	if err != nil {
 		write.WriteHeader(http.StatusConflict)
 		json.NewEncoder(write).Encode(map[string]string{"err": "We were unable to authorise your customer ID! please try again"})
 		return
 	}
+
 	_, err = coll.DeleteOne(read.Context(), bson.M{"_id": id})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! Please try again."})
@@ -364,6 +530,7 @@ func delete(write http.ResponseWriter, read *http.Request) {
 	}
 	write.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(write).Encode(map[string]string{"message": "Success"})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! Please try again."})
@@ -380,15 +547,19 @@ func save(write http.ResponseWriter, read *http.Request) {
 		write.WriteHeader(http.StatusOK)
 		return
 	}
+
 	if read.Method != "POST" {
 		write.WriteHeader(http.StatusMethodNotAllowed)
 		json.NewEncoder(write).Encode(map[string]string{"err": "This method is not allowed"})
 		return
 	}
+
 	defer read.Body.Close()
+
 	var sav rec
 
 	err := json.NewDecoder(read.Body).Decode(&sav)
+
 	if err != nil {
 		write.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(write).Encode(map[string]string{"err": "Invalid data! please input real data."})
@@ -396,25 +567,32 @@ func save(write http.ResponseWriter, read *http.Request) {
 	}
 
 	token := read.URL.Query().Get("token")
+
 	if token == "" {
 		write.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(write).Encode(map[string]string{"err": "We were unable to authorise you! please try again."})
 		return
 	}
+
 	var vet Cust
+
 	if ert := collection.FindOne(read.Context(), bson.M{"token": token}).Decode(&vet); ert != nil {
 		write.WriteHeader(http.StatusUnauthorized)
 		json.NewEncoder(write).Encode(map[string]string{"err": "We were unable to authorise you! please try again."})
 		return
 	}
+
 	_, err = coll.InsertOne(read.Context(), sav)
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! please try again."})
 		return
 	}
+
 	write.WriteHeader(http.StatusOK)
-	err = json.NewEncoder(write).Encode(map[string]string{"message": "Success"})
+	err = json.NewEncoder(write).Encode(map[string]string{"message": "Success deleting data."})
+
 	if err != nil {
 		write.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(write).Encode(map[string]string{"err": "An error occured! please try again."})
@@ -422,27 +600,86 @@ func save(write http.ResponseWriter, read *http.Request) {
 	}
 }
 
+func genOTP(val string, name string, pass string) string {
+	var otp string
+	client := resend.NewClient(os.Getenv("RESEND"))
+
+	n, err := rand.Int(rand.Reader, big.NewInt(900000))
+
+	if err != nil {
+		return "failed to generate OTP"
+	}
+
+	otp = string(int(n.Int64() + 100000))
+
+	type userp struct {
+		User  string `bson:"user"`
+		OTP   string `bson:"otp"`
+		Pass  string `bson:"pass"`
+		Name  string `bson:"name"`
+		Exp   int64  `bson:"exp"`
+		Tries int    `bson:"tries"`
+	}
+
+	rep := userp{
+		User:  val,
+		OTP:   otp,
+		Pass:  pass,
+		Name:  name,
+		Exp:   time.Now().UnixMilli() + 86400000,
+		Tries: 0,
+	}
+
+	_, err = otpCon.InsertOne(context.TODO(), rep)
+
+	if err != nil {
+		return "Failure Generating Response."
+	}
+
+	params := &resend.SendEmailRequest{
+		From:    "Jotter <onboarding@resend.dev>",
+		To:      []string{val},
+		Subject: "User OTP",
+		Html:    "<h2>Hello user!</h2><p>Your User OTP is <b>" + otp + "</b>. Use It to create your user account.</p>",
+	}
+
+	_, err = client.Emails.Send(params)
+
+	if err != nil {
+		return "failed to send"
+	}
+
+	return "success"
+}
+
 func makeToken(val string, read *http.Request) string {
 	const words = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-`"
 	var pes Cust
 	err := collection.FindOne(read.Context(), bson.M{"useName": val}).Decode(&pes)
+
 	if err != nil {
 		return "Invalid"
 	}
+
 	if int64(time.Now().UnixMilli()) < pes.Exp {
 		return pes.Token
 	}
+
 	cont := make([]byte, 20)
 	rand.Read(cont)
 	_, err = collection.UpdateOne(read.Context(), bson.M{"useName": val}, bson.M{"$set": bson.M{"token": string(cont), "exp": int64(time.Now().UnixMilli() + 86400000)}})
+
 	if err != nil {
 		return "Error making token"
 	}
+
 	return string(cont)
 }
+
+// The main function
 func main() {
 	var client *mongo.Client
-	client, err := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGO")))
+	client, err := mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGO"))) //Create a Mongo Client.
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -454,11 +691,13 @@ func main() {
 	}
 	collection = client.Database("shop").Collection("records")
 	coll = client.Database("shop-records").Collection("persons")
+	otpCon = client.Database("OTP").Collection("vars")
 
 	shopIndex := mongo.IndexModel{
 		Keys:    bson.M{"useName": 1},
 		Options: options.Index().SetUnique(true),
 	}
+
 	collection.Indexes().CreateOne(context.TODO(), shopIndex)
 	coll.Indexes().CreateOne(context.TODO(), shopIndex)
 
